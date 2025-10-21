0fb04a0353c173d36add88bdfa3369ad2caf8a3f
romaprime135 <133644519+romaprime135@users.noreply.github.com>
2025-10-22 00:55:06 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
 #include <string>
 
-bool IsKPeriodic(const string& str, int K) {
-    if (K <= 0 || str.length() % K != 0) {
+bool IsKPeriodic ( const std::string &str, int K ) {
+    if ( K <= 0 || str.length ( ) % K != 0 ) {
         return false;
     }
-    
-    string pattern = str.substr(0, K);
-    for (int i = K; i < str.length(); i += K) {
-        if (str.substr(i, K) != pattern) {
+
+    std::string pattern = str.substr ( 0, K );
+    for ( int i = K; i < str.length ( ); i += K ) {
+        if ( str.substr ( i, K ) != pattern ) {
             return false;
         }
     }
-    
+
     return true;
 }
 
-int main() {
-    std::cout << IsKPeriodic("abcabcabcabc", 3) << std::endl;
+int main ( ) {
+    setlocale ( LC_ALL, "RUS" );
+
+    std::cout << ((IsKPeriodic ( "abcabcabcabc", 3 )) ? "Строка переодична" : "Строка хаотична") << std::endl;
+    std::cout << ((IsKPeriodic ( "kvdhahuvfga", 3 )) ? "Строка переодична" : "Строка хаотична") << std::endl;
 
     return 0;
 }
